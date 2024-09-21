7a744a853f1fb1c6fa0776e52f5bee3f3d76c1a6
LSsithu <140079494+LSsithu@users.noreply.github.com>
2024-09-21 09:35:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "rTphRZaI#MfrOZX0KQM2FU9vjG094-07kLhuTJr9zOZvfiX_cna0",
+SESSION_ID: process.env.SESSION_ID || "DBk0FZBS#GDbYY9i-5lBm-Xt0yXK8VxEtqGqrprto1wh5NvugA0M",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/cbf3858a08efefa2d1d47.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖 Hi ꜱᴇɴᴜ Qᴜᴇᴇɴ ʷʰᵃᵗˢᵃᵖᵖ ᵇᵒᵗ Is Online Now 💻\n*💻 Owner* - Rocky \n\n*💻 Owner Number* -94704104383",
 SUDO_NB: process.env.SUDO_NB || "94771665143",
