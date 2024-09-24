0d9054d99959f242843bc4627e59bc95f9284d2e
LSsithu <140079494+LSsithu@users.noreply.github.com>
2024-09-24 08:21:52 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const voice = {
-    alive: 'media/AUD-20240909-WA0003'
+    alive: 'media/WhatsApp Ptt 2024-09-24 at 8.17.11 AM.ogg'
                     }
 let des = `*👋 Hello ${pushname}*
 
